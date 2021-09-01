82fc8621c336b4eb3cb125d47c06df39fbacaa0e
AlexanderWiklow <a.wiklow@gmail.com>
2021-09-01 21:14:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _14th_november
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
